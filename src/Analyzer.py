8d39419a2fc464ff42be7a96d3e44caf331812b4
Hugo Genesse <hugo@pnfsoftware.com>
2018-02-09 01:16:50 -0500

    Parses AST to create UML diagrams.

--- src/Analyzer.py
+++ src/Analyzer.py
@@ -2,18 +2,22 @@
 
 import xml.etree.ElementTree as ET
 from ClassNode import ClassNode
+from FileNode import FileNode
+from AttributeNode import AttributeNode
+from MethodNode import MethodNode
 
 
 class ASTAnalyzer(object):
     """
     Takes the AST dumped and calculates metrics from it.
     """
-    def __init__(self, filename="../MemSan.dump", output_filename="output.txt"):
+    def __init__(self, filename="../MemSan.dump", output_filename="test.dot"):
         self.filename = filename
         self.output_filename = output_filename
         self.AST = None
         self.root = None
         self.currentID = 0
+        self.file = FileNode()
 
     def load_AST(self):
         self.AST = ET.parse(self.filename)
@@ -28,40 +32,39 @@ class ASTAnalyzer(object):
     def parse_class(self):
         for child in self.root:
             if child.tag == "filename":
-                filename = child.text
+                self.file.name = child.text
             elif child.tag == "class":
                 for classChild in child:
                     if classChild.tag == "className":
-                        classname = classChild.text
+                        class_name = classChild.text
+                        new_class = ClassNode(class_name)
+                    elif classChild.tag == "parentClass":
+                        parent_class = classChild.text
+                        if parent_class.find("class ") != -1:
+                            parent_class = parent_class[len("class ") - 1:]
+                        new_class.parent_class = parent_class
                     elif classChild.tag == "method":
-                        nb_ifs = 0
-                        nb_while = 0
-                        nb_break = 0
-                        nb_local_vars = 0
                         for methodChild in classChild:
                             if methodChild.tag == "methodName":
-                                methodName = methodChild.text
-                            elif methodChild.tag == "if":
-                                nb_ifs += 1
-                            elif methodChild.tag == "while":
-                                nb_while += 1
-                            elif methodChild.tag == "break":
-                                nb_break += 1
-                            elif methodChild.tag == "var":
-                                nb_local_vars += 1
-                        output_line = "{},{},{},{},{},{},{},{}".format(
-                            self.currentID,
-                            filename,
-                            classname,
-                            methodName,
-                            nb_ifs,
-                            nb_while,
-                            nb_break,
-                            nb_local_vars
-                        )
-                        self.currentID += 1
-                        self.append_to_output(output_line)
-
+                                method_name = methodChild.text
+                            elif methodChild.tag == "methodReturnType":
+                                method_return_type = methodChild.text
+                        new_method = MethodNode(method_name, method_return_type)
+                        new_class.methods[method_name] = new_method
+                    elif classChild.tag == "attribute":
+                        for attributeChild in classChild:
+                            if attributeChild.tag == "attributeName":
+                                attribute_name = attributeChild.text
+                            elif attributeChild.tag == "attributeType":
+                                attribute_type = attributeChild.text
+                                if attribute_type.find("class ") != -1:
+                                    attribute_type = attribute_type[len("class ") - 1:]
+                                    new_class.classes_used.append(attribute_type)
+                        new_attribute = AttributeNode(attribute_name, attribute_type)
+                        new_class.attributes[attribute_name] = new_attribute
+                self.file.classes[class_name] = new_class
+        with open(self.output_filename, "w") as f:
+            f.write(str(self.file))
 
     def getFilename(self):
         raise
