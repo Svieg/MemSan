f68534747cffabc5b3a049bc86d93212fd69e07c
Hugo Genesse <svieg.ashcasce@gmail.com>
2024-08-27 09:21:56 -0400

    Finish dominator calculation for each basic block

--- src/GhidraAnalyzer.py
+++ src/GhidraAnalyzer.py
@@ -6,7 +6,7 @@ class GhidraAnalyzer():
     def __init__(self):
         self.name = "GhidraAnalyzer"
         self.filename = "cfg/entry.dot"
-        self.basic_blocks = set()
+        self.basic_blocks = {}
         self.edges = []
 
     def load_cfg(self):
@@ -18,40 +18,55 @@ class GhidraAnalyzer():
         matches = regex.findall(dot)
 
         for match in matches:
-            self.basic_blocks.add(BasicBlock(match))
+            self.basic_blocks[match] = BasicBlock(match)
 
-        print(self.basic_blocks)
+        #print(f"Basic blocks: {self.basic_blocks}")
         # load edges
         # matches (bb_4ac) -> (bb_4b1)
         regex = re.compile(b"(bb_.*) -> (bb_.*) \[")
         matches = regex.findall(dot)
 
-        print(matches)
         for match in matches:
+            #print(f"Edge: {match}")
             self.edges.append(Edge(match[0], match[1]))
             self.basic_blocks[match[0]].children.append(match[1])
             self.basic_blocks[match[0]].successors.append(match[1])
             self.basic_blocks[match[1]].parents.append(match[0])
-            self.basic_blocks[match[1]].predecessors.append(match[0])
+            self.basic_blocks[match[1]].predecessors.add(self.basic_blocks[match[0]])
 
     def build_dom_tree(self):
-        changed = False
+        changed = True
         # Initialize every basic block with all the nodes as dominators, except the entry node
         for basic_block_name in self.basic_blocks:
             basic_block = self.basic_blocks[basic_block_name]
-            if basic_block.entry_node:
-                basic_block.dominators.append(basic_block)
+            if len(basic_block.predecessors) == 0:
+                #print(f"Entry node: {str(basic_block)}")
+                basic_block.dominators.add(basic_block)
             else:
-                basic_block.dominators = self.basic_blocks
+                basic_block.dominators = set(self.basic_blocks.values())
         
         # Iterate through all basic blocks and intersect with its predecessors' dominators
-        for basic_block_name in self.basic_blocks:
-            new_doms = []
-            basic_block = self.basic_blocks[basic_block_name]
-            predecessors = basic_block.predecessors
-            for predecessor in predecessors:
-                new_doms = basic_block.dom
+        while changed:
+            changed = False
+            for basic_block_name in self.basic_blocks:
+                #print(f"Basic Block: {basic_block}")
+                temp_dominators = set()
+                basic_block = self.basic_blocks[basic_block_name]
+                temp_dominators.add(basic_block)
+                predecessors = basic_block.predecessors
+                #print(f"Predecessors: {predecessors}")
+                for predecessor in predecessors:
+                    #print(predecessor)
+                    temp_dominators = basic_block.dominators & predecessor.dominators
+                    temp_dominators.add(basic_block)
+                if temp_dominators != basic_block.dominators:
+                    basic_block.dominators = temp_dominators
+                    changed = True
+                #print(f"New doms = {temp_dominators}")
 
+        for basic_block in self.basic_blocks.values():
+            print(basic_block)
+            print(basic_block.dominators)
 
 if __name__ == "__main__":
     ghidra_analyzer = GhidraAnalyzer()
